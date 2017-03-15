8ea1103cfaf0570bd7cf609877e46bdecc3f669f
niusmallnan <zhangzhibo521@gmail.com>
2017-03-15 15:41:48 +0800

    add check bridge exist

--- infra-templates/vxlan/setup_bridge.sh
+++ infra-templates/vxlan/setup_bridge.sh
@@ -2,15 +2,20 @@
 
 set -ex
 
-# Set varibles
-eth0_IP=$(ip addr show eth0 | grep 'inet\b' | awk '{print $2}')
-BASE_MAC=$(ip addr show eth0 | grep ether | awk '{print $2}'| cut -d : -f 4,5,6)
+FLAT_IF=${FLAT_IF:-eth0}
+BRIDGE_NAME=${BRIDGE_NAME:-mpbr0}
 
-BRIDGE_IP=${eth0_IP}
+TEST_BRIDGE=$(ip addr show $BRIDGE_NAME | grep 'inet\b' | awk '{print $2}')
+if [ ! -z $TEST_BRIDGE ]; then
+    exit 0
+fi
+
+# Set varibles
+FLAT_IF_IP=$(ip addr show $FLAT_IF | grep 'inet\b' | awk '{print $2}')
+BASE_MAC=$(ip addr show $FLAT_IF | grep ether | awk '{print $2}'| cut -d : -f 4,5,6)
+BRIDGE_IP=${FLAT_IF_IP}
 BRIDGE_MAC="00:aa:bb:${BASE_MAC}"
 GW_IP=$(ip route show | grep default | awk '{print $3}')
-
-BRIDGE_NAME=mpbr0
 
 ip link add ${BRIDGE_NAME} type bridge
 ip link set ${BRIDGE_NAME} address ${BRIDGE_MAC}
@@ -20,4 +25,3 @@ ip link set dev ${BRIDGE_NAME} up
 ip link set dev eth0 master ${BRIDGE_NAME}
 
 ip route add default via ${GW_IP}
-ip addr show
